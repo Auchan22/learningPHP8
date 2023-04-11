23a16002261e0ffa161663dfea762159091e1c23
Agustin <agustinsurila@gmail.com>
2023-04-11 17:47:30 -0300

    Variables & Constants

---
+++ index.php
@@ -0,0 +1,23 @@
+<!-- <!DOCTYPE html>
+<html>
+<body>
+    <h1>
+        
+        
+        /*
+        $name = "Agustin Surila";
+        echo "Hola {$name}";
+        */
+        
+    </h1>
+</body>
+</html> -->
+
+<?php
+$var1 = 123;
+
+//Definiendo una constante
+//define("name", "value");
+define("STATUS_PAID", 245);
+
+echo STATUS_PAID;
